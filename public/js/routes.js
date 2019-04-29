664a2d250fb1e4654ef945fad8ca1240cf1f72c6
ohrha <trigramthree@gmail.com>
2019-04-30 09:54:29 +1200

    'DISK-SPACE-PAGE'

---
+++ public/js/routes.js
@@ -0,0 +1,64 @@
+(function () {
+
+    console.log("routes.js loaded");
+
+    var app = angular.module("appRoutes", ['ngRoute']);
+
+    app.config(function ($routeProvider, $locationProvider) {
+        //Only providers in config phase... no $rootScope or any other instances...
+
+
+
+        $routeProvider
+        
+            .when('/', {
+                templateUrl: '../home.html',
+                controller: 'mainCtrl',
+                name: "SHINE BRIGHT COACHING | HOME",
+                resolve: {
+                    init: function ($route) {
+                        //console.log("index")
+                    }
+                }
+            })
+            .when('/forms', {
+                templateUrl: '../op.html',
+                name: "SHINE BRIGHT COACHING | FORMS",
+                controller: "mainCtrl",
+                controllerAs: 'main',
+                resolve: {
+                    init: function ($route) {
+                        console.log("index")
+                    }
+                }
+            })
+            
+            .otherwise({
+                redirectTo: '/'
+            });
+
+       $locationProvider.html5Mode({
+            enabled: true,
+            requiredBase: false
+            //now no more # required before routes
+        })
+        
+        
+  
+    })
+
+     app.run(['$rootScope',  function ($rootScope, $routeUpdate, $routeParams) {
+/*
+        $rootScope.$on('$routeChangeStart', function (event, next, current) {
+            //console.log(Auth.isLoggedIn());
+            console.log(next.$$route.name);
+            $rootScope.title = next.$$route.name;
+          
+            //if($routeParams.name == )
+
+        });
+
+*/
+    }]);
+
+}())
