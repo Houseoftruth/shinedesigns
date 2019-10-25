e2fd61d2597fb4360620fbd9025345806187048c
ohrha <trigramthree@gmail.com>
2019-10-25 18:44:21 +1300

    'work'

--- public/js/app.js
+++ public/js/app.js
@@ -67,6 +67,10 @@ app.controller('mainCtrl', ['$http','$scope','$timeout','User','$interval','$win
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
   $scope.data = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+  ];
+  $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
     [28, 48, 40, 19, 86, 27, 90]
   ];
